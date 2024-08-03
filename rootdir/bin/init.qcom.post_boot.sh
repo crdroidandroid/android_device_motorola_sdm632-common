614171a0d79eb892dca6742a1fc198df2472978f
TogoFire <italomellopereira@gmail.com>
2024-08-02 21:31:29 -0300

    sdm632-common: rootdir: Disabled some scheduling knobs
    Test: running jank benchmarks
    Bug: 109836581
    
    Change-Id: Ib4da4f444022f0fc2fe29b99c6afa2165ca1059b
    Signed-off-by: TogoFire <italomellopereira@gmail.com>

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -33,10 +33,6 @@ function 8953_sched_dcvs_eas()
     echo 1 > /sys/devices/system/cpu/cpu0/online
     echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
     echo 0 > /sys/devices/system/cpu/cpufreq/schedutil/rate_limit_us
-    #set the hispeed_freq
-    echo 1401600 > /sys/devices/system/cpu/cpufreq/schedutil/hispeed_freq
-    #default value for hispeed_load is 90, for 8953 and sdm450 it should be 85
-    echo 85 > /sys/devices/system/cpu/cpufreq/schedutil/hispeed_load
 }
 
 function 8917_sched_dcvs_eas()
@@ -45,10 +41,6 @@ function 8917_sched_dcvs_eas()
     echo 1 > /sys/devices/system/cpu/cpu0/online
     echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
     echo 0 > /sys/devices/system/cpu/cpufreq/schedutil/rate_limit_us
-    #set the hispeed_freq
-    echo 1094400 > /sys/devices/system/cpu/cpufreq/schedutil/hispeed_freq
-    #default value for hispeed_load is 90, for 8917 it should be 85
-    echo 85 > /sys/devices/system/cpu/cpufreq/schedutil/hispeed_load
 }
 
 function 8937_sched_dcvs_eas()
@@ -65,11 +57,6 @@ function 8937_sched_dcvs_eas()
     echo 1 > /sys/devices/system/cpu/cpu4/online
     echo "schedutil" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
     echo 0 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/rate_limit_us
-    #set the hispeed_freq
-    echo 768000 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/hispeed_freq
-    #default value for hispeed_load is 90, for 8937 it should be 85
-    echo 85 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/hispeed_load
-
 }
 
 function configure_automotive_sku_parameters() {
