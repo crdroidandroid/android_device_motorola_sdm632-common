Bomb-Projects/device_motorola_sdm632-common

sdm632-common: Dynamic Partitions

BoardConfigCommon.mk
@@ -53,7 +53,10 @@ AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS += \
     boot \
     dtbo \
+    odm \
+    product \
     system \
+    system_ext \
     vendor
 
 # Audio
@@ -170,16 +173,38 @@ TARGET_PROVIDES_LIBLIGHT := true
 ODM_MANIFEST_SKUS += nfc
 ODM_MANIFEST_NFC_FILES := $(PLATFORM_PATH)/configs/vintf/odm_manifest_nfc.xml
 
+# Metadata
+BOARD_USES_METADATA_PARTITION := true
+BOARD_SUPER_PARTITION_METADATA_DEVICE := system
+
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
 BOARD_DTBOIMG_PARTITION_SIZE := 8388608
 BOARD_FLASH_BLOCK_SIZE := 131072
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_USES_RECOVERY_AS_BOOT := true
 TARGET_NO_RECOVERY := true
+TARGET_COPY_OUT_ODM := odm
+TARGET_COPY_OUT_PRODUCT := product
+TARGET_COPY_OUT_SYSTEM := system
+TARGET_COPY_OUT_SYSTEM_EXT := system_ext
+TARGET_COPY_OUT_VENDOR := vendor
+
+# Partitions - Dynamic
+BOARD_SUPER_PARTITION_GROUPS := moto_dynamic_partitions
+BOARD_SUPER_PARTITION_BLOCK_DEVICES := oem system vendor
+BOARD_MOTO_DYNAMIC_PARTITIONS_PARTITION_LIST := odm product system system_ext vendor
+
+# Partitions - FS
+BOARD_ODMIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_SYSTEM_EXTIMAGE_FILE_SYSTEM_TYPE := erofs
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := erofs
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
-TARGET_COPY_OUT_VENDOR := vendor
+
+BOARD_EROFS_COMPRESSOR := lz4
+BOARD_EROFS_PCLUSTER_SIZE := 65536
 
 BOARD_ROOT_EXTRA_SYMLINKS := \
     /vendor/fsg:/fsg

crdroidandroid/android_device_motorola_sdm632-common

BoardConfigCommon.mk
@@ -170,16 +170,29 @@ TARGET_PROVIDES_LIBLIGHT := true
 ODM_MANIFEST_SKUS += nfc
 ODM_MANIFEST_NFC_FILES := $(PLATFORM_PATH)/configs/vintf/odm_manifest_nfc.xml
 
+# Metadata
+BOARD_USES_METADATA_PARTITION := true
+BOARD_SUPER_PARTITION_METADATA_DEVICE := system
+
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
 BOARD_DTBOIMG_PARTITION_SIZE := 8388608
 BOARD_FLASH_BLOCK_SIZE := 131072
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_USES_RECOVERY_AS_BOOT := true
 TARGET_NO_RECOVERY := true
+TARGET_COPY_OUT_SYSTEM := system
+TARGET_COPY_OUT_VENDOR := vendor
+
+# Partitions - Dynamic
+BOARD_SUPER_PARTITION_GROUPS := moto_dynamic_partitions
+BOARD_SUPER_PARTITION_BLOCK_DEVICES := oem system vendor
+BOARD_MOTO_DYNAMIC_PARTITIONS_PARTITION_LIST := system vendor
+
+# Partitions - FS
+BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
-TARGET_COPY_OUT_VENDOR := vendor
 
 BOARD_ROOT_EXTRA_SYMLINKS := \
     /vendor/fsg:/fsg